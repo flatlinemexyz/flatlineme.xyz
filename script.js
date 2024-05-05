4f525e5d445cf7bfc80d0501a84b9564a84b5913
Flatline <164840549+flatlinemexyz@users.noreply.github.com>
2024-05-06 03:46:09 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-// Initialize dynamic effects when the document is fully loaded
 document.addEventListener('DOMContentLoaded', () => {
     const headerText = document.querySelector('.header-text');
 
     function changeTextColor() {
         const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
-        headerText.style.transition = 'color 1s ease-in-out'; // Smooth transition
+        headerText.style.transition = 'color 1s ease-in-out';
         headerText.style.color = randomColor;
     }
 
-    setInterval(changeTextColor, 2000); // Change color every 2000 milliseconds
+    setInterval(changeTextColor, 2000);
 });
